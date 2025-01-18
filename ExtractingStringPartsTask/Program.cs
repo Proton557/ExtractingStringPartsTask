516fe1b7c584620a2a7610f674694a27fa2be7e2
Pratham Khanna <22PKhanna@heckgrammar.co.uk>
2025-01-18 13:14:02 +0000

    hmf

--- ExtractingStringPartsTask/Program.cs
+++ ExtractingStringPartsTask/Program.cs
@@ -19,11 +19,8 @@ namespace ExtractingStringPartsTask
             // output a concatenation of the first and last characters of city
             Console.WriteLine($"{city[0]}{city[state.Length-1]}");
             // output the substring of landmark starting at c and outputting to the end
-            Console.WriteLine(landmark[2]);
             // output the substring of country starting at the first S and ending at the first A
 
-            // output the position of f in California
-            //Console.WriteLine(state.IndexOf());
             //************CHALLENGE****************//
             // Output how many vowels are in each of the strings in the format
             // California has n vowels
