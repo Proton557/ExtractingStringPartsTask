9a10615c4a65d1b8438aa7b1ef7a68dc1003e751
Pratham Khanna <22PKhanna@heckgrammar.co.uk>
2025-01-20 22:05:26 +0000

    Completed Added ReadMe

--- ExtractingStringPartsTask/Program.cs
+++ ExtractingStringPartsTask/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
 
 namespace ExtractingStringPartsTask
 {
@@ -10,23 +12,36 @@ namespace ExtractingStringPartsTask
             string state = "California";
             string city = "San Fransicso";
             string landmark = "Alcatraz Island";
-            // output the length of state
-            Console.WriteLine(state.Length);
-            // Concatenate the contents of city and landmark * don't forget to add a space
-            Console.WriteLine($"{city} {landmark}");
-            // output the first character of country
-            Console.WriteLine(country[0]);
-            // output a concatenation of the first and last characters of city
-            Console.WriteLine($"{city[0]}{city[state.Length-1]}");
-            // output the substring of landmark starting at c and outputting to the end
-            // output the substring of country starting at the first S and ending at the first A
+            // output the length of state --------------------- DONE
+            Console.WriteLine("1 " + state.Length);
+            // Concatenate the contents of city and landmark * don't forget to add a space --------------------- DONE
+            Console.WriteLine($"2 {city} {landmark}");
+            // output the first character of country --------------------- DONE
+            Console.WriteLine("3 " + country[0]);
+            // output a concatenation of the first and last characters of city --------------------- DONE
+            Console.WriteLine($"4 {city[0]}{city[12]}");
+            // output the substring of landmark starting at c and outputting to the end --------------------- DONE
+            Console.WriteLine($"5 {landmark.Substring(landmark.IndexOf('c'))}");
+            // output the substring of country starting at the first S and ending at the first A --------------------- DONE
 
-            //************CHALLENGE****************//
-            // Output how many vowels are in each of the strings in the format
-            // California has n vowels
+            int i = 7;
+            while (i<18)
+            {
+                Console.WriteLine($"{country[i]}");
+                
+                i++;
+            }
+            // output the position of f in California --------------------- DONE
+            char searchChar = 'f';
+            int position = state.IndexOf(searchChar);
 
-
+            if (position != -1)
+            {
+                Console.WriteLine($"The position of {searchChar} in {state} is: {position+1}");
+            }
             
+
+
 
             // Show your evidence of the program running in the Readme file
         }
